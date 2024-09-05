e275dd4d87620e18759c066c34623fc05fea1589
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-05 21:28:07 +0200

    add elif and print

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@ for i in range(10):
     for j in range(i + 1, 10):
         if i == 8 and j == 9:
             print(f"{i}{j}")
+        elif i == 0 and j == 1:
+            print(f"{i}{j}", end="")
         else:
-            print(f"{i}{j}", end=", ")
+            print(f", {i}{j}", end="")
