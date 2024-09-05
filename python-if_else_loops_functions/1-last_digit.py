91c5e88dd128aa9200861a73bedaf99ec55d71be
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-05 14:57:39 +0200

    change number by count in conditions

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-count = random.randint(-10, 10)
-if number > 5:
+count = number % 10
+if count > 5:
     print(f"Last digit of {number} is {count} and is greater than 5")
-elif number == 0:
+elif count == 0:
     print(f"Last digit of {number} is {count} and is 0")
 else:
     print(f"Last digit of {number} is {count} and is less than 6 and not 0")
