9be93b396246f6e7b7f5ac5a4dcb9d74604e64da
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-06 15:22:48 +0200

    Modifying value 4 by 5

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(1,101):
+    for num in range(1, 101):
         string = " "
         if num % 3 == 0:
             string = string + "Fizz"
-        if num % 4 == 0:
+        if num % 5 == 0:
             string = string + "Buzz"
-        if num % 4 != 0 and num % 3 != 0:
+        if num % 5 != 0 and num % 3 != 0:
             string = string + str(num)
         print(string, end="")
