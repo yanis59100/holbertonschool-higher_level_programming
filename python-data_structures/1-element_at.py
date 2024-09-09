05288a862ccfb8bd144de49cedbd952ce9866ecd
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-09 12:40:11 +0200

    Modifying elif

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     for i in my_list:
         if idx < 0:
             return None
-        elif idx > 6:
+        elif idx > len(my_list):
             return None
         else:
             return my_list[3]
