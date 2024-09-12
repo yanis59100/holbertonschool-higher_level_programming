dd216faf004e4af74e88062e9291cb6db95484c8
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-12 18:56:48 +0200

    Write a function that replaces an element of a list at a specific position.

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    my_list[3] = 9
-    for i in my_list:
-        if idx < 0:
-            return None
-        elif idx > len(my_list):
-            return None
-        else:
-            return my_list
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+    return (my_list)
