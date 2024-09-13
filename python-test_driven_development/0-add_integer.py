ca487b581fb5dca8928e26de3a5ba2c1128311b4
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-13 19:38:39 +0200

    Correction pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,10 +3,9 @@ def add_integer(a, b=98):
     """ Add new integer or floats a and b value 98
 
     If a and b are not integers or floats
-    
 
     Return integer a + interger b
-    """    
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
