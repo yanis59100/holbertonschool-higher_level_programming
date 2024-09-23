543ce24e640f2285744e8fd67c3303dacd053606
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-23 14:34:52 +0200

    Write a class BaseGeometry (based on 5-base_geometry.py).

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Contains the class BaseGeometry"""
+class BaseGeometry:
+    """A class with public attribute area"""
+    def area(self):
+        raise Exception ("area() is not implemented")
