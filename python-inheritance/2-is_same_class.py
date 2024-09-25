d1702fb60d5402a88d8a28b2bea3c24da24aafd4
yanis59100 <meftouh.yanis@hotmail.com>
2024-09-25 14:38:24 +0200

    Corrected pycodestyle

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """This module contains the function is_same_class"""
+
+
 def is_same_class(obj, a_class):
     """This module contains the function is_same_class"""
-    return type(obj) == a_class
+    return type(obj) is a_class
