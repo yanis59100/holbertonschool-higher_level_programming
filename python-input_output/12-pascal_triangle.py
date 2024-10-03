c2c1c02814ff3cc9bf47cbfb33013d18a4f977f9
yanis59100 <meftouh.yanis@hotmail.com>
2024-10-03 20:16:24 +0200

    Create a function that returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""defines function creates and representing pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """interger representing triangle pascal"""
+    if n <= 0:
+        return []
+    triangle = [[1]]
+
+    for i in range(1, n):
+        """range integer in triangle pascal"""
+        prev_row = triangle[-1]
+        new_row = [1]
+        for j in range(1, len(prev_row)):
+            new_row.append(prev_row[j-1] + prev_row[j])
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
