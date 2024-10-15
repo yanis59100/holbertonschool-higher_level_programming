5de83f59d9dc008f0ac3b82f35d5aabf4f997daf
yanis59100 <meftouh.yanis@hotmail.com>
2024-10-15 13:03:43 +0200

    Modifying code

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -15,12 +15,11 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
-    r = requests.get(url)
-    if r.status_code == 200:
-        data = r.json()
-        posts = [
-        {"id": 1, "title": "First post", "body": "This is the first post."},
-        {"id": 2, "title": "Second post", "body": "This is the second post."},
-        ]
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        posts = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in data]
         with open("posts.csv", "w", newline='') as file:
-            file.write(str(data))
+            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            writer.writerows(posts)
