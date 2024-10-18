8b81fda7e4f6460ee1641d99cedce442fe8fde12
yanis59100 <meftouh.yanis@hotmail.com>
2024-10-18 20:41:27 +0200

    Modifying coding

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,35 +6,39 @@ import json
 PORT = 8000
 
 
-class serveur(http.server.BaseHTTPRequestHandler):
+class Server(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
         elif self.path == '/data':
             self.send_response(200)
-            json_string = '{"name": "John", "age": 30, "city": "New York"}'
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(json_string).encode())
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
         elif self.path == '/status':
             self.send_response(200)
-            json_string = "404 Not Found status"
-            self.wfile.write(json.dumps(json_string).encode())
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            self.wfile.write(json.dumps(json_string).encode())
+            status_message = {"status": "404 Not Found"}
+            self.wfile.write(json.dumps(status_message).encode())
         elif self.path == '/info':
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            json_strings = '{"version": "1.0","description": "A simple API built with http.server"}'
-            self.wfile.write(json.dumps(json_strings).encode())
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.wfile.write(json.dumps(info).encode())
+
         else:
             self.send_error(404)
-            self.wfile.write(b"404")
 
 
-with socketserver.TCPServer(("", PORT), serveur) as httpd:
-    print("serving at port", PORT)
+with socketserver.TCPServer(("", PORT), Server) as httpd:
+    print(f"Serving at port {PORT}")
     httpd.serve_forever()
