435916ebe5efd9a2818a3df9e55c0045256a8514
yanis59100 <meftouh.yanis@hotmail.com>
2024-10-18 22:30:32 +0200

    Modifying condition

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,25 +1,30 @@
-#!/usr/bin/python3
 import requests
 import csv
 
 
 def fetch_and_print_posts():
-    url = "https://jsonplaceholder.typicode.com/posts"
-    r = requests.get(url)
-    print("Statue code: {}".format(r.status_code))
-    if r.status_code == 200:
-        posts = r.json()
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
         for post in posts:
-            print(post["title"])
-
+            print(post['title'])
+    else:
+        print("Failed to retrieve posts")
 
 def fetch_and_save_posts():
-    url = "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(url)
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
-        data = response.json()
-        posts = [{"id": post["id"], "title": post["title"], "body": post["body"]} for post in data]
-        with open("posts.csv", "w", newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
+        posts = response.json()
+
+        post_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+
+        with open('posts.csv', 'w', newline='') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
-            writer.writerows(posts)
+            writer.writerows(post_data)
+
+        print("Posts saved to posts.csv")
+    else:
+        print("Failed to retrieve posts")
