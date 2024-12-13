5aa106bb906ba923196611f47b13babf4daeda32
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-13 17:10:09 +0100

    Corrected condition

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 """Displays all cities of a given state from thestates table of the database hbtn_0e_4_usa."""
+from MySQLdb import connect
 import sys
-import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    db = connect(host="localhost",
+                 port=3306,
+                 user=sys.argv[1],
+                 passwd=sys.argv[2],
+                 db=sys.argv[3],
+                 charset="utf8"
+                 )
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM cities where state_id =
+                (SELECT id FROM states WHERE name = '{}')
+                """.format(sys.argv[4]))
+    cities = [row[0] for row in cur.fetchall()]
+    print(", ".join(cities))
+    cur.close()
+    db.close()
