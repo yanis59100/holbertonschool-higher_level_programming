74262533bbe7ed6f1d8e724f778265f086ce261c
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-13 16:21:58 +0100

    Accessing http://localhost:5000 should show the message: "Welcome to the Flask API!".

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,49 +1,45 @@
 #!/usr/bin/python3
-from flask import Flask
-from flask import jsonify
-from flask import request
+
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 users = {}
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return "Welcome to the Flask API"
+    return "Welcome to the Flask API!"
 
-@app.route('/data')
-def jsonfy():
-    global users
-    users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
-    return jsonify({'users': users})
 
-@app.route('/status')
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")
 def status():
-    return jsonify({"status": "OK"})
+    return "OK"
 
-@app.route('/users/<username>')
-def name(username: str):
+
+@app.route("/users/<username>")
+def get_user(username):
     user = users.get(username)
     if user:
         return jsonify(user)
     else:
         return jsonify({"error": "User not found"}), 404
 
-@app.route('/add_user', methods=['POST'])
-def register():
-    data = request.get_json()
 
-    username = data['username']
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    user_data = request.get_json()
+    username = user_data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
 
-    users[username] = {
-        "username": username,
-        "name": data.get('name', ''),
-        "age": data.get('age', ''),
-        "city": data.get('city', '')
-    }
-
-    return jsonify({"message": "User added", "user": users[username]})
 
 if __name__ == "__main__":
     app.run()
