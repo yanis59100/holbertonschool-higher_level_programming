f2b2bbd6113c4a9811b012a869dbc62ba132cc96
yanis59100 <meftouh.yanis@hotmail.com>
2024-12-13 17:14:18 +0100

    Corrected condition

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max_num = my_list[0]
-    for i in my_list:
-        if i > max_num:
-            max_num = i
-            return max_num
+    if len(my_list):
+        max = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
+    return None
